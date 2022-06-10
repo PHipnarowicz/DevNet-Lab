83e436a3a7ae194d2edc3bca34e7b70381cdc861
PHipnarowicz <piotr.hipnarowicz@gmail.com>
2022-06-10 12:17:42 +0200

    Add script1.py

---
+++ Nornir/Lab1/script1.py
@@ -0,0 +1,16 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+
+
+
+def main():
+
+    nr = InitNornir(config_file="config.yaml")
+
+    results = nr.run(task=send_command, command="show ip interfaces brief")
+    print_result(results)
+
+
+if __name__ == "__main__":
+    main()
